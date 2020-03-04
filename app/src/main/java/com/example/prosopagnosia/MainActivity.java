65b727d0d27eb1d536bd297d0cb186e137d83c02
Melishua <panmelissa@hotmail.com>
2020-03-04 12:14:24 -0500

    first commit

---
+++ app/src/main/java/com/example/prosopagnosia/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.prosopagnosia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Real-Time Recognition Mode button */
+    public void enterRealTimeRecognitionMode() {
+
+    }
+}
